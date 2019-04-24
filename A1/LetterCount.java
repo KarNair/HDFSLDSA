8417adc876c636f0001ecfd7266e08b6eb1ccaf4
KarNair <karthikkuttan@gmail.com>
2019-04-24 23:24:54 +0200

    Letter Count V2

--- A1/LetterCount.java
+++ A1/LetterCount.java
@@ -25,6 +25,8 @@ public class WordCount {
     }
 }	
     public static class Reduce extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
+        private LongWritable result = new LongWritable();
+
         public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
             int sum = 0;
             while (values.hasNext()) {
