db5658b24c0b22fe905ae0743bfca886e6edf558
KarNair <karthikkuttan@gmail.com>
2019-04-25 23:19:48 +0200

    NewCode

--- A1/FirstLetterCount.java
+++ A1/FirstLetterCount.java
@@ -1,4 +1,3 @@
-//package org.myorg;
 import java.io.IOException;
 import java.util.*;
 import org.apache.hadoop.fs.Path;
@@ -7,52 +6,57 @@ import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapred.*;
 import org.apache.hadoop.util.*;
 
-public class LetterCount {
+
+public class WordCount {
     public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
         private final static IntWritable one = new IntWritable(1);
-        private Text letter = new Text();
-	    
-        public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
+        private Text word = new Text();
+
+        public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter)
+                throws IOException {
             String line = value.toString();
             StringTokenizer tokenizer = new StringTokenizer(line);
             while (tokenizer.hasMoreTokens()) {
-                String str = tokenizer.nextToken().startsWith("[^a-zA-z]").toLowerCase();
-                for (int i = 0; i<str.length(); i++){
-                   letter.set(new byte[] {(byte)str.charAt(i)});
-                   output.collect(letter,one);
+                // Convert string to upper case in order to make the mapping
+                // function not case-sensitive
+                String token = tokenizer.nextToken().toUpperCase();
+                // Only look at the first letter
+                Character letter = token.charAt(0);
+                // Produce the word using the first letter of the string
+                word.set("number_of_" + String.valueOf(letter));
+                output.collect(word, one);
             }
         }
     }
-}	
-    public static class Reduce extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
-        private IntWritable result = new IntWritable();
 
-        public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
+
+    public static class Reduce extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
+        public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output,
+                Reporter reporter) throws IOException {
             int sum = 0;
             while (values.hasNext()) {
                 sum += values.next().get();
-                result.set(sum);
-                output.collect(key, result);
             }
+            output.collect(key, new IntWritable(sum));
         }
     }
-	
+
     public static void main(String[] args) throws Exception {
         JobConf conf = new JobConf(WordCount.class);
         conf.setJobName("wordcount");
-	    
+
         conf.setOutputKeyClass(Text.class);
         conf.setOutputValueClass(IntWritable.class);
-	    
+
         conf.setMapperClass(Map.class);
         conf.setReducerClass(Reduce.class);
-	    
+
         conf.setInputFormat(TextInputFormat.class);
         conf.setOutputFormat(TextOutputFormat.class);
-	    
+
         FileInputFormat.setInputPaths(conf, new Path(args[0]));
         FileOutputFormat.setOutputPath(conf, new Path(args[1]));
-	    
+
         JobClient.runJob(conf);
     }
 }
