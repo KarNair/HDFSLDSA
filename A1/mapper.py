ac1f5d9743adfc8f669e72d8f31f1245acc9dab3
KarNair <karthik.nair1194@gmail.com>
2019-04-26 17:30:52 +0000

    Final Code

--- A1/mapper.py
+++ A1/mapper.py
@@ -10,31 +10,28 @@ pronoun = ["han","hen","hon","den","det","denna","denne"]
 
 # input comes from STDIN (standard input)
 for line in sys.stdin:
-	line = line.strip()
-    tweets = json.loads(line)
+    line = line.strip()
 
     try:
-    	tweets = json.loads(line)
-    	if 'retweeted_status' in tweets:
-    		if retweeted_status!='':
-    			continue
+        tweets = json.loads(line)
+        if 'retweeted_status' in tweets:
+            if retweeted_status!='':
+                continue
+        #print ('Unique_Tweets', 1)
+        print ('%s\t%s' % ('Unique_Tweets', 1))
 
-    	print ('Unique_Tweets', 1)
-
-    	text = tweets["text"]
+        text = tweets["text"]
 
     	#flag = 0
 
-    	for word in pronoun:
+        for word in pronoun:
+            text_match = re.compile(r"\b%s\b" %(word), re.IGNORECASE)
+            if text_match.search(text):
+                print ('%s\t%s' % (word, 1))
+                #flag = 1
 
-    		text_match = re.compile(r"\b%s\b" %(item), re.IGNORECASE)
-    		
-    		if text_match.search(text):
-    			print(word,1)
-    			#flag = 1
-
-    	"""if flag == 1:
-    		print('keywords_unique',1)"""
+#if flag == 1:
+#print('keywords_unique',1)
     except:
-    	pass
+        pass
 
